f9d3852352934eb38849e33190d6cdf7e3e5be98
RaOnee <87575326+RaOnee@users.noreply.github.com>
2021-07-17 20:23:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var num = Math.floor(Math.random()*6)+1;
+var imagesource = "images/dice"+num+".png";
+document.querySelectorAll("img")[0].setAttribute("src",imagesource);
+
+var num2 = Math.floor(Math.random()*6)+1;
+var imagesource2 = "images/dice"+num2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",imagesource2);
+
+if(num>num2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(num<num2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw";
+}
